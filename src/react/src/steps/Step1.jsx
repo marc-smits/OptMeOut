1aa6b7913d9b61fdf1f85ffad71a5a154edb8f91
Marc Smits <hello@marcsmits.eu>
2025-07-31 16:58:06 +0200

    [feat] SVG arrow in TellMeMore button + CSS

--- src/react/src/steps/Step1.jsx
+++ src/react/src/steps/Step1.jsx
@@ -119,6 +119,9 @@ function Step1(props) {
                                 {!tellMeMore &&
                                         <div className="button buttonTellMeMore" onClick={tellMore}>
                                                 [[button.tellmemore]]
+                                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M7.875 13.6969L7.875 -4.86836e-07L10.125 -3.7865e-07L10.125 13.6969L16.425 7.39688L18 9L9 18L-3.57639e-07 9L1.575 7.39688L7.875 13.6969Z" fill="currentcolor"></path>
+                                                </svg>
                                         </div>
                                 }
                                 <div className="button buttonOptMeOut"
