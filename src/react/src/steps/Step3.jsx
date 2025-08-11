4f8058efd411a5dc68314a16bb6abb7518a72c4b
Marc Smits <hello@marcsmits.eu>
2025-08-11 22:51:54 +0200

    [feat] reworked validation output in HTML

--- src/react/src/steps/Step3.jsx
+++ src/react/src/steps/Step3.jsx
@@ -1,7 +1,7 @@
 /**
- * 
+ *
  * Section Step3
- * 
+ *
  */
 import PropTypes from 'prop-types';
 import React, { useState } from 'react'
@@ -12,54 +12,42 @@ import BackSvg from './partials/BackSvg.jsx';
 
 
 // Todo for Marc, move classes .formFieldLabelError and .formFieldError  to your place
-import './step2.scss'
 
 function Step3(props) {
 
     let propsFormData = props.formData;
 
-    const [styleFirstNameField, setStyleFirstNameField] = useState('')
-    const [styleFirstNameFieldLabel, setStyleFirstNameFieldLabel] = useState('')
-
-    const [styleLastNameField, setStyleLastNameField] = useState('')
-    const [styleLastNameFieldLabel, setStyleLastNameFieldLabel] = useState('')
-
-    const [styleTelephoneField, setStyleTelephoneField] = useState('')
-    const [styleTelephoneFieldLabel, setStyleTelephoneFieldLabel] = useState('')
-
+    const [validFirstName, setValidFirstName  ] = useState(true)
+    const [validLastName, setValidLastName] = useState(true)
+    const [validPhone, setValidPhone] = useState(true)
 
     const [dateOfBirth, setDateOfBirth] = useState(new Date());
-    const [styleDateOfBirthField, setStyleDateOfBirthField] = useState('')
-    const [styleDateOfBirthFieldLabel, setStyleDateOfBirthFieldLabel] = useState('')
+    const [validDateOfBirth, setValidDateOfBirth] = useState(true)
     const [dateOfBirthIsSet, setDateOfBirthIsSet] = useState(false);
 
-
-    const [styleBsnField, setStyleBsnField] = useState('')
-    const [styleBsnFieldLabel, setStyleBsnFieldLabel] = useState('')
-
-    const [formIsValid, setFormIsValid] = useState(true)
-
+    const [validBsn, setValidBsn] = useState(true)
+    // const [formIsValid, setFormIsValid] = useState(true)
+    let formIsValid = true;
 
     /*
     *
     *  Handle change of a text field
-    * 
+    *
     */
     const handleChange = (e) => {
         props.emitUpdateFormdata(e.target.name, e.target.value);
     };
 
-
     /*
     *
     *  Change date field
-    * 
+    *
     */
     const changeDate = (date) => {
         let day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
         let month = date.getMonth() > 8 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1);
         let year = date.getFullYear();
-        
+
         let dateStr = day + '/' + month + '/' + year;
         setDateOfBirthIsSet(true);
         props.emitUpdateFormdata('dateOfBirth', dateStr);
@@ -69,66 +57,43 @@ function Step3(props) {
         setDateOfBirth(new Date(dateStr));
     };
 
-
     /*
     *
     *  Validate form
-    * 
+    *
     */
     const validateForm = (e) => {
-
-        setStyleFirstNameField('');
-        setStyleFirstNameFieldLabel('');
-        setStyleLastNameField('');
-        setStyleLastNameFieldLabel('');
-        setStyleTelephoneField('');
-        setStyleTelephoneFieldLabel('');
-        setStyleDateOfBirthField('');
-        setStyleDateOfBirthFieldLabel('');
-        setStyleBsnField('');
-        setStyleBsnFieldLabel('');
-        setFormIsValid(true);
-
-        if (
-            propsFormData.firstName != '' &&
-            propsFormData.lastName != '' &&
-            propsFormData.telephone != '' &&
-            (dateOfBirthIsSet || props.formData.dateOfBirth != '')
-        ) {
-            props.emitChangeSection("step4", e);
-        } else {
-            setFormIsValid(false);
-            if (propsFormData.firstName == '') {
-                setStyleFirstNameField('formFieldError');
-                setStyleFirstNameFieldLabel('formFieldLabelError');
+            if (!propsFormData.firstName) {
+                setValidFirstName(false);
+                formIsValid = false;
             }
-            if (propsFormData.lastName == '') {
-                setStyleLastNameField('formFieldError');
-                setStyleLastNameFieldLabel('formFieldLabelError');
+            if (!propsFormData.lastName) {
+                setValidLastName(false);
+                formIsValid = false;
             }
-
-            if (propsFormData.telephone == '') {
-                setStyleTelephoneField('formFieldError');
-                setStyleTelephoneFieldLabel('formFieldLabelError');
+            if (!propsFormData.telephone) {
+                setValidPhone(false);
+                formIsValid = false;
             }
-
             if (!dateOfBirthIsSet && props.formData.dateOfBirth == '') {
-                setStyleDateOfBirthField('formFieldError');
-                setStyleDateOfBirthFieldLabel('formFieldLabelError');
+                setValidDateOfBirth(false);
+                formIsValid = false;
+            }
+            if (!propsFormData.bsn) {
+                setValidBsn(false);
+                formIsValid = false;
             }
 
-            if (propsFormData.bsn == '') {
-                setStyleBsnField('formFieldError');
-                setStyleBsnFieldLabel('formFieldLabelError');
+            if(formIsValid) {
+              console.log("we can continue: " + formIsValid);
+              props.emitChangeSection("step4", e);
             }
-        }
     };
-
 
     /*
     *
     * Open Privacy policy
-    * 
+    *
     */
     const openPrivacyPolicy = (e) => {
             props.emitChangeSection("privacyPolicy", e);
@@ -137,87 +102,85 @@ function Step3(props) {
     return (
 
         <div className="step" id="Step3">
-            {/* Todo for Marc, make a class for this */}
-            <div style={{
-                textAlign: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundImage: "url(" + "/src/assets/img/step2.svg    " + ")",
-                width: '229px',
-                margin: 'auto'
-            }}
-            >&nbsp;</div>
+
+        <div>&nbsp;</div>
 
             {/* Intro */}
             <h1 className="headline">[[step3.title]]</h1>
+            <div className="progress bar2" >&nbsp;</div>
+            <p className="intro">[[step3.intro]]</p>
 
-            <p className="intro">
-                [[step3.intro]]
-            </p>
-            <p className="intro">
-                <p style={{ color: 'red' }}>
-                    {formIsValid ? "" : "[[global.form.error]]"}
-                </p>
-                <span className={'classes generic Input Labels By Marc ' + styleFirstNameFieldLabel}> [[step3.form.field1.label]]</span>
-                <input
-                    className={'classes generic Input By Marc ' + styleFirstNameField}
-                    value={propsFormData.firstName}
-                    name="firstName"
-                    placeholder="[[step3.form.field1.value]]"
-                    onChange={handleChange}
-                />
+            <div className='formRow'>
+              <div className="col">
+                <fieldset>
+                    <p className="formError">
+                        {formIsValid ? "" : "[[global.form.error]]"}
+                    </p>
+                    <div>
+                      <label data-valid={validFirstName}> [[step3.form.field1.label]]</label>
+                      <input
+                          data-valid={validFirstName}
+                          // value={propsFormData.firstName}
+                          name="firstName"
+                          placeholder="[[step3.form.field1.value]]"
+                          onChange={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <label data-valid={validLastName}>[[step3.form.field2.label]]</label>
+                      <input
+                          data-valid={validLastName}
+                          // value={propsFormData.lastName}
+                          name="lastName"
+                          placeholder="[[step3.form.field2.value]]"
+                          onChange={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <label data-valid={validPhone}>[[step3.form.field3.label]]</label>
+                      <input
+                          data-valid={validPhone}
+                          // value={propsFormData.telephone}
+                          name="telephone"
+                          placeholder="[[step3.form.field3.value]]"
+                          onChange={handleChange}
+                      />
+                    </div>
+                    <div>
+                      <label data-valid={validDateOfBirth}>[[step3.form.field4.label]]</label>
 
-                <br />
-                <span className={'classes generic Input Labels By Marc ' + styleLastNameFieldLabel}> [[step3.form.field2.label]]</span>
-                <input
-                    className={'classes generic Input By Marc ' + styleLastNameField}
-                    value={propsFormData.lastName}
-                    name="lastName"
-                    placeholder="[[step3.form.field2.value]]"
-                    onChange={handleChange}
-                />
+                      <DatePicker
+                          name="dateOfBirth"
+                          data-valid={validDateOfBirth}
+                          dateFormat="dd/MM/YYYY"
+                          selected={dateOfBirth}
+                          showYearDropdown
+                          yearDropdownItemNumber={100}
+                          scrollableYearDropdown
+                          onChange={changeDate}
+                      />
+                    </div>
+                    <div>
+                      <label data-valid={validBsn}>[[step3.form.field5.label]]</label>
+                      <input
+                          data-valid={validBsn}
+                          value={propsFormData.bsn}
+                          name="bsn"
+                          placeholder="[[step3.form.field5.value]]"
+                          onChange={handleChange}
+                      />
+                    </div>
 
-                <br />
-                <span className={'classes generic Input Labels By Marc ' + styleTelephoneFieldLabel}> [[step3.form.field3.label]]</span>
-                <input
-                    className={'classes generic Input By Marc ' + styleTelephoneField}
-                    value={propsFormData.telephone}
-                    name="telephone"
-                    placeholder="[[step3.form.field3.value]]"
-                    onChange={handleChange}
-                />
-
-                <br />
-                <span className={'classes generic Input Labels By Marc ' + styleDateOfBirthFieldLabel}> [[step3.form.field4.label]]</span>
-
-                <DatePicker
-                    name="bsn"
-                    className={'classes generic Input By Marc ' + styleDateOfBirthField}
-                    dateFormat="dd/MM/YYYY"
-                    selected={dateOfBirth}
-                    showYearDropdown
-                    yearDropdownItemNumber={100}
-                    scrollableYearDropdown
-                    onChange={changeDate}
-                />
-
-                <br />
-                <span className={'classes generic Input Labels By Marc ' + styleBsnFieldLabel}> [[step3.form.field5.label]]</span>
-                <input
-                    className={'classes generic Input By Marc ' + styleBsnField}
-                    value={propsFormData.bsn}
-                    name="bsn"
-                    placeholder="[[step3.form.field5.value]]"
-                    onChange={handleChange}
-                />
-            </p>
-
+                </fieldset>
+              </div>{/*col*/}
+            </div>{/*formRow*/}
 
             <div className='row'>
                 <div className="col flex-center">
 
-                    <div className="button buttonTellMeMore" onClick={(e) => props.emitChangeSection("step2", e)}>
-                        [[button.back]]
+                    <div className="button buttonBack" onClick={(e) => props.emitChangeSection("step2", e)}>
                         <BackSvg/>
+                        [[button.back]]
                     </div>
 
                     <div className="button buttonOptMeOut" onClick={(e) => validateForm()}>
@@ -225,8 +188,7 @@ function Step3(props) {
                     </div>
                 </div>{/*col*/}
             </div>{/*row*/}
-        </div>
-
+          </div>
     )
 }
 
