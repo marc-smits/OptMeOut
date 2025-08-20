f75e96bb72e08eb9b53225ac8934e368d15c3c5a
cptuulia <tantonius@icloud.com>
2025-08-20 13:23:29 +0200

    translations for step 4

--- src/react/src/steps/Step4.jsx
+++ src/react/src/steps/Step4.jsx
@@ -21,7 +21,35 @@ function Step4(props) {
             <PDFViewer>
                 <EmailPdf formData={props.formData} />
             </PDFViewer>
-
+<pre>
+                step4.months.january : [[step4.months.january]] <br/> 
+                step4.months.february : [[step4.months.february]] <br/> 
+                step4.months.march: [[step4.months.march]] <br/> 
+                step4.months.april: [[step4.months.april]] <br/> 
+                step4.months.may: [[step4.months.may]] <br/> 
+                step4.months.june: [[step4.months.june]] <br/> 
+                step4.months.july: [[step4.months.july]] <br/> 
+                step4.months.august: [[step4.months.august]] <br/> 
+                step4.months.september: [[step4.months.september]] <br/> 
+                step4.months.october: [[step4.months.october]] <br/> 
+                step4.months.november: [[step4.months.november]] <br/> 
+                step4.months.december: [[step4.months.december]] <br/> 
+                step4.receive_a_copy.title : [[step4.receive_a_copy.title]] <br/> 
+                step4.receive_a_copy.text: [[step4.receive_a_copy.text]] <br/> 
+                step4.receive_a_copy.email_place_holder: [[step4.receive_a_copy.email_place_holder]] <br/> 
+                step4.opt_out_letter.title: [[step4.opt_out_letter.title]] <br/> 
+                step4.opt_out_letter.saluation: [[step4.opt_out_letter.saluation]] <br/> 
+                step4.opt_out_letter.content: [[step4.opt_out_letter.content]] <br/> 
+                step4.donate.title: [[step4.donate.title]] <br/> 
+                step4.donate.text: [[step4.donate.text]] <br/> 
+                step4.donate.payment_options.1: [[step4.donate.payment_options.1]] <br/> 
+                step4.donate.payment_options.2: [[step4.donate.payment_options.2]] <br/> 
+                step4.donate.payment_options.3: [[step4.donate.payment_options.3]] <br/> 
+                step4.donate.payment_options.4: [[step4.donate.payment_options.4]] <br/> 
+                 button.send: [[button.send]] <br/> 
+</pre>
+  
+               
             <div className='row'>
                 <div className="col flex-center">
                     <div className="button buttonBack" onClick={(e) => props.emitChangeSection("step3", e)}>
