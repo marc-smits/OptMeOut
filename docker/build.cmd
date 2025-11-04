e29ea22e1e9188d6401221af0ee3ea826c6bfe99
Thijs Houtenbos <github@thd.ninja>
2025-11-04 13:09:56 +0100

    Fixed docker build scripts

--- docker/build.cmd
+++ docker/build.cmd
@@ -1,3 +1,6 @@
 @echo off
 
+REM Execute in docker folder
+cd %~dp0
+
 docker compose -p optmeout run --rm build
--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Execute in docker folder
+cd "$(dirname "$0")"
+
 docker compose -p optmeout up --force-recreate --wait -d
