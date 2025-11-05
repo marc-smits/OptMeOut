8259e2beafbc66fe19d3dd762e33106c2e8064d4
Thijs Houtenbos <github@thd.ninja>
2025-11-05 15:18:05 +0100

    Added PHP BE compose install step

---
+++ phpApi/api/index.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>OptMeOut API</title>
+</head>
+
+<body>
+    <h1>OptMeOut API</h1>
+    <p><a href="/en_GB">Continue to the website</a></p>
+</body>
+
+</html>
--- phpApi/api/vendor/composer/installed.php
+++ phpApi/api/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-OPME-76-pingen-confirmation-mail',
-        'version' => 'dev-OPME-76-pingen-confirmation-mail',
-        'reference' => '45bb549179e9f815ba0506d80f0aa135ef12a439',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-OPME-76-pingen-confirmation-mail',
-            'version' => 'dev-OPME-76-pingen-confirmation-mail',
-            'reference' => '45bb549179e9f815ba0506d80f0aa135ef12a439',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
